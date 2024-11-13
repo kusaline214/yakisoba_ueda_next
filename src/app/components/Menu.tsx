0622ece0a3e6708eacd59f0a67041797a2e20ee8
kusaline214 <dreamdumper@gmail.com>
2024-11-13 23:41:37 +0900

    文字中央寄せ

--- src/app/components/Menu.tsx
+++ src/app/components/Menu.tsx
@@ -15,14 +15,20 @@ const Menu = () => {
   return (
     <ConceptBox className=" p-5 flex flex-col justify-center md:w-[800px] mx-auto">
       <h2 className="text-xl text-center my-5">MENU</h2>
-      <p className="text-sm leading-7">
+      <p className="text-sm leading-7 md:text-xl md:text-center">
         初めての方は絶品の焼きそばをご賞味下さい！
         <br />
         生卵を絡めて食べるのもお楽しみ下さい
       </p>
-      <div className="flex justify-between my-[30px]">
-        <Image src={"/logo.JPG"} alt={""} width={200} height={200} />
-        <Image src={"/logo.JPG"} alt={""} width={200} height={200} />
+      <div className="md:flex md:justify-center my-[30px] md:items-center">
+        <div>
+          <Image src={"/logo.JPG"} alt={""} width={200} height={200} />
+          <p>商品名</p>
+        </div>
+        <div>
+          <Image src={"/logo.JPG"} alt={""} width={200} height={200} />
+          <p>商品名</p>
+        </div>
       </div>
       <button className="w-[50%] md:w-[250px] text-xs mx-auto bg-black text-white py-2 my-[10px] rounded-md">
         メニューはこちらから
